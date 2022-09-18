4303c8540ae747bf6704f8d79c558b66ae2461c9
Mihu <solinuxnet@gmail.com>
2022-09-18 23:05:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{value === 11 ? setValue(0) : value}</h1>
+        <button onClick={() => setValue(value - 1)} className="minus">
+          - Минус
+        </button>
+        <button onClick={() => setValue(value + 1)} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
